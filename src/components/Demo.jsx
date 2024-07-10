eccb94520f441ba9b7aca607c92202db1bffb4ac
Zahid Hasan <zahidhasan140338@gmail.com>
2024-07-10 13:52:52 +0600

    the copy button is working fine

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
@@ -9,11 +9,12 @@ const Demo = () => {
   });
 
   const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
 
-    // RTK lazy query
-    const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  // RTK lazy query
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
-    // Load data from localStorage on mount
+  // Load data from localStorage on mount
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
       localStorage.getItem("articles")
@@ -23,7 +24,6 @@ const Demo = () => {
       setAllArticles(articlesFromLocalStorage);
     }
   }, []);
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -35,87 +35,98 @@ const Demo = () => {
 
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
-      
 
-      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
-  }
-}
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
 
-
+  // copy the url and toggle the icon for user feedback
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
 
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
-          <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5"/>
-          <input type="url" placeholder="Enter a URL" value={article.url} onChange={(e) => setArticle({ ...article, url: e.target.value })} required className="url_input peer"/>
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
           <button
-            type='submit'
-            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
           >
             <p>↵</p>
           </button>
         </form>
 
         {/* Browse URL History */}
-       <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
-        {allArticles.map((item, index) =>(
-          <div
-            key={`link-${index}`}
-            onClick={() => setArticle(item)}
-            className="link_card"
-          >
-            <div className="copy_btn">
-              <img src={copy} alt="copy_icon" 
-              className="w-[40%] h-[40%] object-contain"
-              />
-
-            </div>
-            <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img
+                  src={copied === item.url ? tick : copy}
+                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
                 {item.url}
               </p>
-
-          </div>
-        ))} 
-
-       </div>
-       
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Display Results */}
-      <div className='my-10 max-w-full flex justify-center items-center'>
+      <div className="my-10 max-w-full flex justify-center items-center">
         {isFetching ? (
-            <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
         ) : error ? (
-          <p className='font-inter font-bold text-black text-center'>
-          Well, that wasn't supposed to happen...
-          <br />
-          <span className='font-satoshi font-normal text-gray-700'>
-            {error?.data?.error}
-          </span>
-        </p>
+          <p className="font-inter font-bold text-black text-center">
+            Well, that was not supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
         ) : (
           article.summary && (
-            <div className='flex flex-col gap-3'>
-                <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
-                Article <span className='blue_gradient'>Summary</span>
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
               </h2>
-              <div className='summary_box'>
-              <p className='font-inter font-medium text-sm text-gray-700'>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
                   {article.summary}
                 </p>
-
               </div>
-
             </div>
           )
         )}
-
       </div>
-
     </section>
-  )
+  );
 };
-
 
 export default Demo;
